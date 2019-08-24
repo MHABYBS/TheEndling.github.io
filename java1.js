ab9d6b1874c4cb033511b8081d5b3b7a8f6b24b2
TheEndling <47886931+TheEndling@users.noreply.github.com>
2019-08-25 01:51:13 +0200

    Update java1.js

--- java1.js
+++ java1.js
@@ -3,3 +3,8 @@ function showIt() {
     document.getElementById("hide").style.visibility = "visible";  
   }  
   setTimeout("showIt()", 5000); 
+
+  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if (!isChrome) {
+      $('#iframeAudio').remove()
+    }

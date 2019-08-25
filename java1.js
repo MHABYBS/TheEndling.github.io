37c2a3e9c6a32d06c336acde26503e105bcc6679
TheEndling <47886931+TheEndling@users.noreply.github.com>
2019-08-25 02:05:50 +0200

    Update java1.js

--- java1.js
+++ java1.js
@@ -4,7 +4,25 @@ function showIt() {
   }  
   setTimeout("showIt()", 5000); 
 
-  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-    if (!isChrome) {
-      $('#iframeAudio').remove()
+  // Loads the YouTube IFrame API JavaScript code.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/watch?v=WeXUgF2qEuk&t=13s";
+// Inserts YouTube JS code into the page.
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+// onYouTubeIframeAPIReady() is called when the IFrame API is ready to go.
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    videoId: 'LS-ErOKpO4E', // the ID of the video (mentioned above)
+    playerVars: {
+      autoplay: 1, // start automatically
+      controls: 0, // don't show the controls (we can't click them anyways)
+      modestbranding: 1, // show smaller logo
+      loop: 1, // loop when complete
+      playlist: 'kNizPk7xBbs' // required for looping, matches the video ID
     }
+  });
+}
